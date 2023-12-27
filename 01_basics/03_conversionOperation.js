f53d34b15485863e553e64ef88862f1c0eefb79b
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2023-12-27 05:56:13 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "23"
+let score2 = "23ABS"
+let score3 = null
+let score4;
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+let valueInNumber2 = Number(score2)
+let valueInNumber3 = Number(score3)
+let valueInNumber4 = Number(score4)
+console.log(typeof(valueInNumber));
+console.log(typeof(valueInNumber2));
+console.log(typeof(valueInNumber3));
+console.log(typeof(valueInNumber4));
+// console.log(valueInNumber2)
+// console.log(valueInNumber3)
+// console.log(valueInNumber4)
+
+// "33" -> 33
+// "33abc" -> NaN
+
+let isLoggedIn = 1
+
+let isLoggedInBool = Boolean(isLoggedIn)
+console.log(isLoggedIn);
+
+console.log(typeof isLoggedInBool)
+console.log(isLoggedInBool);
+
+/*
+    1 -> true
+    0 -> false
+    "" -> false
+    "Karan" -> true
+*/
