970faae2abbf47e97693582e676733109c0bd83a
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2023-12-27 05:44:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using node.js not browser
+
+console.log(3 + 3);
+console.log("Karan");
+
+
+let name = "Karan" // String
+let age = 18 // Integer
+let isLoggedIn = false // Boolean
+
+let state // undefined
+let city = null; // standalone value
+
+console.log(typeof city);  // typeof returns the type of the value
