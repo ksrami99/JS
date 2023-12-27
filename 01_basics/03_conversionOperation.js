376a8a7716c057cb48e2182e155132623eb8eb06
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2023-12-27 06:14:03 +0000

    operations in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,18 +1,18 @@
-let score = "23"
-let score2 = "23ABS"
-let score3 = null
-let score4;
+// let score = "23"
+// let score2 = "23ABS"
+// let score3 = null
+// let score4;
 
 // console.log(typeof score);
 
-let valueInNumber = Number(score)
-let valueInNumber2 = Number(score2)
-let valueInNumber3 = Number(score3)
-let valueInNumber4 = Number(score4)
-console.log(typeof(valueInNumber));
-console.log(typeof(valueInNumber2));
-console.log(typeof(valueInNumber3));
-console.log(typeof(valueInNumber4));
+// let valueInNumber = Number(score)
+// let valueInNumber2 = Number(score2)
+// let valueInNumber3 = Number(score3)
+// let valueInNumber4 = Number(score4)
+// console.log(typeof(valueInNumber));
+// console.log(typeof(valueInNumber2));
+// console.log(typeof(valueInNumber3));
+// console.log(typeof(valueInNumber4));
 // console.log(valueInNumber2)
 // console.log(valueInNumber3)
 // console.log(valueInNumber4)
@@ -20,13 +20,13 @@ console.log(typeof(valueInNumber4));
 // "33" -> 33
 // "33abc" -> NaN
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let isLoggedInBool = Boolean(isLoggedIn)
-console.log(isLoggedIn);
+// let isLoggedInBool = Boolean(isLoggedIn)
+// console.log(isLoggedIn);
 
-console.log(typeof isLoggedInBool)
-console.log(isLoggedInBool);
+// console.log(typeof isLoggedInBool)
+// console.log(isLoggedInBool);
 
 /*
     1 -> true
@@ -34,3 +34,33 @@ console.log(isLoggedInBool);
     "" -> false
     "Karan" -> true
 */
+
+
+// ------------------- operations -------------------------
+
+let value = 3;
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2); // pov
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Karan"
+
+console.log(str1 + str2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+
+let gaemCounter = 100;
+gaemCounter++
+console.log(gaemCounter)
