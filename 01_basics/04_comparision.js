2133a0ed33125e8834f7941b00c47a57368f9eb5
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2023-12-27 06:23:47 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 >= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// avoid this all conversions
+console.log("2" > 1);
+console.log("02" > 1);
+
+// avoid this all conversions
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// === 
+
+console.log("2" === 2);
