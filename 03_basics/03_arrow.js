7045601c48fba57c88a94fb8b6c380fdd2c92915
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2024-01-19 10:56:17 +0000

    arrow function and this keyword

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,39 @@
+const user = {
+    username:"Karan",
+    price:999,
+
+    walecomeMessage : function(){
+        console.log(`${this.username}, Walecome to website`);
+        console.log(this);
+    }
+}
+
+// user.walecomeMessage()
+// user.username = "sam"
+// user.walecomeMessage()
+
+// console.log(this);
+
+// function one(){
+//     console.log(this);
+// }
+// one()
+
+// const chai = () => {
+//     let username = "Karan"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// const addTwo = (num1, num2) => return num1 + num2;
+// const addTwo = (num1, num2) => (num1 + num2)
+const addTwo = (num1, num2) => ({username: "Karan"})
+
+
+// console.log(addTwo(2,3));
+
