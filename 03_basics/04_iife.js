ff52eac7604b37f5eafec8303603f289d266d1e0
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2024-01-19 11:09:44 +0000

    iife in js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+// function chai(){
+//     console.log(`DB CONNECTED`);
+// }
+// chai()
+
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+})(); // it will run imidiatly
+
+((name) => {
+    console.log(`${name}`);
+})("Karan");
