93978c71a8ad7ad008df8e805a10e1fc6af6c1ab
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2024-01-20 06:32:40 +0000

    conditionals in js

---
+++ 04_control_flow/conditional.js
@@ -0,0 +1,62 @@
+// if
+
+if(true){
+    console.log("true");
+}
+
+if(true){
+    // ya to ye run hoga
+}else{
+    // ya ye !
+}
+
+// condition ? true : false  // ternary operator
+
+const balance = 500;
+if(balance < 500){
+
+}else if (balance == 500){
+
+}else {
+
+}
+
+const userLoggedIn = true
+const debitCard = true
+
+if(userLoggedIn && debitCard){
+    console.log("You can buy anything !");
+}
+
+
+
+// switch
+
+const month = 1;
+
+switch(month){
+    case 1:
+        console.log("Jan");
+        break;
+    
+    case 2:
+        console.log("Fab");
+        break;
+    
+    case 3:
+        console.log("Mar");
+        break;
+    
+    case 4:
+        console.log("Apr");
+        break;
+    
+    case 5:
+        console.log("Jun");
+        break;
+
+    default:
+        console.log("NUN");
+        break;
+    
+}
